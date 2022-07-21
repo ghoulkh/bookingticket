10af6a1d5949dfb13c9fcf4d3ea713a7a6c6cfa2
ghoulkh <91310945+ghoulkh@users.noreply.github.com>
2022-07-21 20:08:41 +0700

    Update quanlyve.php

--- quanlyve.php
+++ quanlyve.php
@@ -4,7 +4,12 @@
 <title>Webserver mua vé với Raspberry Pi</title>
 </head>
     <body>
+    <form action="tinhtoan.php" method="post">
        <center><h1>Webserver mua vé với Raspberry Pi</h1>
+       <?php 
+       shell_exec("gpio -g mode 27 out");
+       shell_exec("gpio -g write 27 0");
+       ?>   
         <form action="tinhtoan.php" method="POST">
             Từ thành phố: <br>
             <select name="city1">
@@ -13,10 +18,10 @@
                 <option value="3">Hồ Chí Minh</option>
                 <option value="4">Cần Thơ</option>
             </select>
-        </center>
+        </center>    
+
         <center>
-        <form action="" method="post">
-            Từ thành phố: <br>
+            Đến thành phố: <br>
             <select name="city2">
                 <option value="1">Hà Nội</option>
                 <option value="2">Đà Nẵng</option>
@@ -24,6 +29,7 @@
                 <option value="4">Cần Thơ</option>
             </select>
         </center>
+
         <center>
         <button type="submit" name="submit">Gửi</button>
         </center>
